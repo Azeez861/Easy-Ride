7669e878e7dca9eb3c5ff5ed111d558fe39c4249
Azeez861 <amuhammedakinkunmi@gmail.com>
2026-07-14 23:26:08 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+let splash = document.getElementById("splash");
+let mainCountent = document.getElementById("main-content");
+
+setTimeout(function () {
+
+    splash.style.display = "none";
+
+    mainCountent.style.display = "block";
+}, 2500);
+
+const text = "Easy Ride.";
+const typingElement = document.getElementById("typing-text");
+
+let index = 0;
+
+function typeWriter() {
+    if (index < text.length) {
+        typingElement.textContent += text.charAt(index);
+        index++;
+        setTimeout(typeWriter, 100)
+    }
+}
+
+
+window.onload = () => {
+    typingElement.textContent = "";
+    index = 0;
+    typeWriter();
+};
+
+
+
+
+const slider = document.querySelector(".slider");
+
+let currentSlide = 0;
+const totalSlides = 5;
+
+setInterval(() => {
+
+    currentSlide++;
+
+    if(currentSlide >= totalSlides){
+        currentSlide = 0;
+    }
+
+    slider.style.transform = `translateX(-${currentSlide * 100}vw)`;
+
+}, 2000); // 10000ms = 10 seconds
